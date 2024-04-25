4e5cf40ec5a870f0f65f0139f8c1b1c41315d3ce
alexhiguera <alexhigueramartinez@gmail.com>
2024-04-25 23:19:10 +0200

    update: building structs and includes

--- src/philo.h
+++ src/philo.h
@@ -3,17 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: columbux <columbux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 01:51:54 by alex              #+#    #+#             */
-/*   Updated: 2024/04/04 02:41:31 by alex             ###   ########.fr       */
+/*   Updated: 2024/04/25 23:18:21 by columbux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include "../Libft_2.0/src/libft.h"
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdio.h>
 
+typedef struct s_philo_args
+{
+	int		n_philos;
+	int		time_die;
+	int		time_eat;
+	int		time_sleep;
+	int		n_eat;
+}				t_args;
+
+typedef struct s_philo_stats
+{
+	int		id;
+	int		n_ate;
+	int		death;
+}				t_stas;
 
 #endif
