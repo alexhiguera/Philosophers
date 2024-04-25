f7a83c599b9db4f5f8987d77904cb965b695969d
alexhiguera <alexhigueramartinez@gmail.com>
2024-04-26 01:19:42 +0200

    update: atoi function

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: columbux <columbux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 23:27:48 by columbux          #+#    #+#             */
-/*   Updated: 2024/04/25 23:39:46 by columbux         ###   ########.fr       */
+/*   Updated: 2024/04/26 01:01:11 by columbux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,30 @@ void	ph_error(char *message)
 	white = "\033[0;97m";
 	printf("%sError: %s%s\n", red, message, white);
 	exit(EXIT_FAILURE);
+}
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r' || str[i] == '\f'
+		|| str[i] == '\n' || str[i] == '\v')
+		i++;
+	sign = 1;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	result = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * result);
 }
