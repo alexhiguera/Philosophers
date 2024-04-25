54cda4538f99d37bd55f0e302a86c53249089a14
alexhiguera <alexhigueramartinez@gmail.com>
2024-04-25 23:48:21 +0200

    update: args error  handler

--- src/philo.c
+++ src/philo.c
@@ -6,7 +6,7 @@
 /*   By: columbux <columbux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 19:50:18 by columbux          #+#    #+#             */
-/*   Updated: 2024/04/25 22:40:27 by columbux         ###   ########.fr       */
+/*   Updated: 2024/04/25 23:45:41 by columbux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,11 @@ void	*philosophers(void *data)
 
 int	main(int argc, char **argv)
 {
-	if (argc == 5)
+	if (argc == 6)
 	{
-		//TODO
+	//TODO
 	}
+	else
+		ph_error("5 arguments required\n ./philo <num_philos> <time_to_die> \
+		<time_to_eat> <time_to_sleep> <num_of_times_each_philos_must_eat>");
 }
